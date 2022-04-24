fd20a1f3d39b55c9cb804505b932ab76a2c59877
Nick Melanson <nickm890@gmail.com>
2022-04-24 11:24:04 -0400

    added model columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,43 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references Product model id
+      references: {
+        model: Product,
+        key: "id",
+      },
+    },
+    // define tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references Tag model id
+      references: {
+        model: Tag,
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
